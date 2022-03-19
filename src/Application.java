a443f2805007ae0065143728fbcf10c8b558c8c5
DevBalek <muhammedfurkanbalek@gmail.com>
2022-03-19 19:16:59 +0300

    read file added.

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,11 @@
+import java.io.File;
 
 public class Application {
 	public static void main(String[]args) {
+		File file =new File("Deneme.java");
 		
+		if(file.canRead()) {
+			
+		}
 	}
 }
--- src/Operators.java
+++ src/Operators.java
@@ -1,6 +1,25 @@
+import java.io.File;
 
 public class Operators {
-
+	
+	private File file;
+	
+	Operators(File file){
+		this.file = file;
+	}
+	
+	int numericOperators() {
+		file.canExecute();
+		return 0;
+	}
+	
+	int relationalOperators() {		
+		return 0;
+	}
+	
+	int logicalOperators() {
+		return 0;
+	}
 }
 
 
