d3e2fe3087b55137965f3715b18a5b4de69375bf
DevBalek <muhammedfurkanbalek@gmail.com>
2022-03-19 18:17:01 +0300

    initial files

---
+++ src/Operators.java
@@ -0,0 +1,43 @@
+
+public class Operators {
+
+}
+
+
+/*
+
+	Sayısal Operator
+	+
+	++
+	-
+	--
+	*
+	/
+	%
+	&
+	|
+	^
+	=
+	+=
+	-=
+	/=
+	*=
+	%=
+	&=
+	|=
+	^=
+	
+	İlişkisel Operator
+	<
+	<=
+	>
+	>=
+	==
+	!=
+	
+	Mantıksal Operator
+	&&
+	||
+	!
+
+*/
